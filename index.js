63a7e8214ce1a43848dd28f27ae348b5aa0b2d26
DeveloperZeal <sohansth@proton.me>
2025-12-30 09:06:39 +0545

    feature to store employee's data through localstorage, and rendering employees

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const addEmployeeBtn = document.getElementById("add-employee-btn");
 const popupForm = document.getElementById("popup-employee-form");
 const employeeForm = document.getElementById("employee-form");
 const employeeListEl = document.getElementById("employee-list");
+const noEmployeeMsgEls = document.getElementsByClassName("no-employee-msg");
 
-let employeeList = [];
+let employeeList = JSON.parse(localStorage.getItem("employees")) || [];
+renderEmployees();
+
+if (employeeList.length > 0) {
+  for (let el of noEmployeeMsgEls) {
+    el.style.display = "none";
+  }
+}
 
 // Show popup form when Add Employee button is clicked
 addEmployeeBtn.addEventListener("click", () => {
@@ -18,11 +26,10 @@ popupForm.addEventListener("click", (e) => {
   }
 });
 
-
 employeeForm.addEventListener("submit", (e) => {
-  e.preventDefault()
-  const data = new FormData(popupForm);
-  
+  e.preventDefault();
+  const data = new FormData(employeeForm);
+
   const employee = {
     firstName: data.get("first-name"),
     secondName: data.get("second-name"),
@@ -31,21 +38,29 @@ employeeForm.addEventListener("submit", (e) => {
     phone: data.get("phone"),
     address: data.get("address"),
     photo: data.get("photo"),
-  }
-  
+  };
+
   employeeList.push(employee);
-  renderEmployees();
+  localStorage.setItem("employees", JSON.stringify(employeeList));
   
+  renderEmployees();
+
   employeeForm.reset();
   popupForm.classList.remove("show");
-
 });
 
 // Remove popup form when pressed "Escape"
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    popupForm.classList.remove("Show");
+    popupForm.classList.remove("show");
   }
 });
 
-
+function renderEmployees() {
+  employeeListEl.innerHTML = "";
+  employeeList.forEach((employee, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${employee.firstName} ${employee.secondName} <br> ${employee.post}`;
+    employeeListEl.appendChild(li);
+  });
+}
